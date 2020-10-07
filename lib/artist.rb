cd3e9fc3d84766d0f6d848b6ebc06c3c3fef717a
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-07 23:59:53 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,12 +2,12 @@ class Artist
   attr_accessor :name, :songs
   
   @@song_count = 0
-  @@all = []
+  #@@all = []
   
   def initialize(name)
     @name = name
     @songs = []
-    @@all << self
+    #@@all << self
   end
   
   def songs
