d04d35f5f7791be49b03f87256c9e4af760282b8
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-08 00:23:07 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,19 +1,19 @@
 class Author 
   attr_accessor :name, :post, :title
   
-  @@count = 0
-  @@all = []
+  @@post_count = 0
+  #@@all = []
   
   def initialize(name)
     @name = name
     @posts = post
     @@count += 1
-    @@all << self
+    #@@all << self
   end
   
-  #def posts= (posts)
-   # @posts = posts
-  #end 
+  def posts
+   Post.all.select {|post| post.author == self}
+ end
   
   def title= (title)
     @title = title
