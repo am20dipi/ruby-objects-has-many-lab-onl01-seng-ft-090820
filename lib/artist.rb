d2d89a0d89b506d8da313be5078a27a43e66418a
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-08 00:08:34 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,19 +12,16 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self#this is telling a song that it belongs to an artist; self keyword refers to the artist on which we are calling this method
-    @@song_count +=1
+    @@song_count += 1
   end
   
-  def self.all
-    @@all
-  end
   
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
     add_song(song)
-    @@song_count +=1
+    @@song_count += 1
   end
   
   def self.song_count
